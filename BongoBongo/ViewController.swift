1a8b31117ad4d3134c333a50a6e71d1cbb9b3b36
Levi Lunique <levi.lunique@gmail.com>
2021-10-14 21:35:42 -0400

    ViewController and SquareBehavior

--- BongoBongo/ViewController.swift
+++ BongoBongo/ViewController.swift
@@ -9,11 +9,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var board: UIView!{
+        didSet{
+            animator = UIDynamicAnimator(referenceView: board)
+            behavior = SquareBehavior()
+            animator?.addBehavior(behavior!)
+        }
+    }
+    
+    private var animator: UIDynamicAnimator?
+    private var behavior: SquareBehavior?
+    private var squares = [UIView]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onSquare(_ sender: UIButton) {
+        
+        var frame = CGRect(origin: .zero, size: CGSize(width: 50, height: 50))
+        let x = arc4random() % UInt32(board.bounds.size.width)
+        frame.origin.x = CGFloat(x)
+        
+        let square = UIView(frame: frame)
+        square.backgroundColor = UIColor.cyan
+        board.addSubview(square)
+        squares.append(square)
+        behavior?.addItem(square)
+        
+    }
+    
+    @IBAction func onExplode(_ sender: UIButton) {
+        guard squares.count > 0 else {return}
+        
+        squares.forEach({behavior?.removeItem($0)})
+        
+        UIView.animate(withDuration: 1){
+            
+            self.explodeSquares()
+            
+        } completion: { (finished) in
+            self.squares.forEach({$0.removeFromSuperview()})
+            self.squares.removeAll()
+        }
+        
+    }
+    
+    private func explodeSquares(){
+        for sq in self.squares{
+            let x = arc4random() % UInt32(self.board.bounds.size.width)
+            let y = self.board.bounds.size.height
+            sq.center = CGPoint(x: CGFloat(x), y: -y)
+        }
+    }
+    
 }
 
