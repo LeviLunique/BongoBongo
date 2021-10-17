08332785afa211cd3cc0b3e210b2fc90ce372de4
Levi Lunique <levi.lunique@gmail.com>
2021-10-17 19:31:50 -0400

    Timer

--- BongoBongo/ViewController.swift
+++ BongoBongo/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    var timeRemaining: Int = 10
+    var timer: Timer!
+    var initalizeTimer = false
+    
+    
     @IBOutlet weak var board: UIView!{
         didSet{
             let tap = UITapGestureRecognizer(target: self, action: #selector(onTap(_:)))
@@ -31,6 +38,37 @@ class ViewController: UIViewController {
         
         
     }
+    
+    private func playTimer(){
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(step), userInfo: nil, repeats: true)
+    }
+    
+    @objc func step(){
+        if timeRemaining > 0 {
+            timeRemaining -= 1
+        } else {
+            timer.invalidate()
+            timeRemaining = 10
+            startExplosion()
+            playTimer()
+            
+        }
+        label.text = "\(timeRemaining)"
+    }
+    private func checkTimer(){
+        if initalizeTimer == false {
+            playTimer()
+            initalizeTimer = true
+        }
+    }
+    private func resetTimer(){
+            timer.invalidate()
+            timeRemaining = 10
+            label.text = "\(timeRemaining)"
+        playTimer()
+    }
+    
+    
     private func createSquare(at point: CGPoint){
         let frame = CGRect(origin: point, size: CGSize(width: 30, height: 30))
         
@@ -41,6 +79,7 @@ class ViewController: UIViewController {
             blue: CGFloat.random(in: 0...1),
             alpha: 1)
         
+        checkTimer()
         board.addSubview(square)
         squares.append(square)
         behavior?.addItem(square)
@@ -60,6 +99,11 @@ class ViewController: UIViewController {
     }
     
     @IBAction func onExplode(_ sender: UIButton) {
+        startExplosion()
+        resetTimer()
+    }
+    
+    private func startExplosion(){
         guard squares.count > 0 else {return}
         
         squares.forEach({behavior?.removeItem($0)})
@@ -71,6 +115,7 @@ class ViewController: UIViewController {
         } completion: { (finished) in
             self.squares.forEach({$0.removeFromSuperview()})
             self.squares.removeAll()
+            
         }
         
     }
@@ -81,6 +126,8 @@ class ViewController: UIViewController {
             let y = self.board.bounds.size.height
             sq.center = CGPoint(x: CGFloat(x), y: -y)
         }
+        
+        
     }
     
 }
